e951d7197d35f855efb9a19c6f3e6f99eee21358
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-29 12:05:38 -0500

    JAVA_08

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-let pago= document.querySelector("#pago").value;
-    if (pago>130000){
-        let total= pago-(pago*0.15);
-        document.querySelector("#total").innerHTML= 'El total a pagar es de${total} y se le palica un descuento del 15%';
+let dato= document.querySelector('#dato');
+dato.addEventListener("change", ()=>{
+    let Selection= dato.value;
+    let boton= document.querySelector('#form');
+    boton.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let sueldoEmp= pagar(Selection);
+        document.querySelector('#total').innerHTML= sueldoEmp;
+    })
+})
+function pagar(Selection){
+    let horas= document.querySelector('#horas').value;
+    if (Selection=='planta'){
+        const sueldo= 20000;
+        let pagaTotal=sueldo*horas;
+        return pagaTotal;
     } else{
-        document.querySelector("#total").innerHTML= 'El total a pagar es de ${pagar}';
+        alert("Debes seleccionar un cargo")
     }
+}

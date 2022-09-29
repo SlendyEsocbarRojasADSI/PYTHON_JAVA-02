eaf6ab24954a43636020913b376fe72316dcab94
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-29 11:41:26 -0500

    JAVA_06, ahora si perdon

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-addEventListener("DOMContentLoaded", (e)=>{
-    let total= document.querySelector("#form");
-    total.addEventListener("submit", (e)=>{
-        e.preventDefault();
-        
-        let pago= document.querySelector("#pago").value;
-        if (pago>130000){
-            let total= pago-(pago*0.15);
-            document.querySelector("#total").innerHTML= 'El total a pagar es de${total} y se le palica un descuento del 15%';
-        } else{
-            alert("normal")
-            document.querySelector("#total").innerHTML= 'El total a pagar es de ${pagar}';
-        }
-    })
-})
+let tempe= Number(prompt("Ingresar la temperatura:"));
+let presion= Number(prompt("Ingresar la presion:"));
+    if (tempe>200 && presion>100){
+        alert("alarma");
+    }
+    else{
+        alert("normal");
+    }

09093d9d47ec7177e60a2204d6dd1edbf66855e2
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 07:39:14 -0500

    JAVA_13

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-let num1= Number(prompt("Ingresar el primer numero:"));
-let num2= Number(prompt("Ingresar el segundo numero:"));
-    if (num1>num2){
-        let resta= num2-num1;
-        alert("Debido a que el numero " +num1+ " es mayor que " +num2+ " se resto y dio con un total de: " +resta);
+let A= 0.10;
+let B= 0.15;
+let C= 0.20;
+
+let membre= prompt("Ingresar su categoria de membresia"+"A, B, C");
+    if(membre==="A"){
+        let price= Number(prompt("Ingresar el costo de su helado:"));
+        let descu= price*A;
+        let total= price-descu;
+
+        alert("Por tener una membresia tipo A, recibiste un descuento del" +A+ "%, el precio que tendras que pagar por tu helado es de: $" +total);
     }
-    else {
-        alert("No es posible hacer esta operadiocn");
+    else if (membre===B){
+        let price= Number(prompt("Ingresar el costo de su helado:"));
+        let descu= price*B;
+        let total= price-descu;
+
+        alert("Por tener una membresia tipo B, recibiste un descuento del" +B+ "%, el precio que tendras que pagar por tu helado es de: $" +total);
+    }
+    else if (membre===C){
+        let price= Number(prompt("Ingresar el costo de su helado:"));
+        let descu= price*C;
+        let total= price-descu;
+
+        alert("Por tener una membresia tipo C, recibiste un descuento del" +C+ "%, el precio que tendras que pagar por tu helado es de: $" +total)
     }
